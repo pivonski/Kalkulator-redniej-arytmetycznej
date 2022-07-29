8eb93fda9fed683a863d947bf249428c42a0e48c
Jakub Piwoński <Jakub.Piwonski@microsoft.wsei.edu.pl>
2022-07-29 17:35:25 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp22/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program służący do liczenia średniej arytmetycznej");
+
+            float a;
+            float sum = 0;
+            int i = 0;
+
+            do
+            {
+                Console.WriteLine("Podaj liczbę: ");
+                a = float.Parse(Console.ReadLine());
+                sum += a;
+                i++;
+
+            }
+            while (a != 0);
+
+            sum = (float)Math.Round(sum / (i-1), 2);
+            Console.WriteLine($"Średnia arytmetyczna to: {sum}");
+        }
+    }
+}
